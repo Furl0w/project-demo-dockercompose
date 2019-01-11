48ae80b177e2a61a24b5767447a56546430142ec
Furl0w <rainbowphoque@gmail.com>
2019-01-11 11:07:00 +0100

    adding test mobile function for dev test

--- serverMain/main.go
+++ serverMain/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -19,7 +20,7 @@ func main() {
 	})
 
 	http.HandleFunc("/checkDB", checkDB)
-
+	http.HandleFunc("/testMobile", testMobile)
 	http.ListenAndServe(":"+PORT, nil)
 }
 
@@ -42,4 +43,17 @@ func checkDB(w http.ResponseWriter, r *http.Request) {
 		response.Body.Read(p)
 		fmt.Fprintf(w, "Response : %s", string(p))
 	}
+}
+
+func testMobile(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "error when receiving stuff\n")
+		log.Println(err.Error())
+		return
+	}
+	w.Header().Set("Content-type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(body))
 }
